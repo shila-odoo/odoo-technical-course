e99016654213058bcd20f1f0b383faec35f6bf66
shila-odoo <shila@odoo.com>
2026-03-24 12:50:41 +0400

    name change

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default';
 import { defineUserConfig } from 'vuepress';
 
 export default defineUserConfig({
-  base: '/',
+  base: '/real-estate-course',
   title: 'VuePress Starter',
   description: 'VuePress Starter Project in Stackblitz',
 
